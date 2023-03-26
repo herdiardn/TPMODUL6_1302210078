9b7159446a3cd25f1a2d45a67dfa4e0781183e3b
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-03-26 14:15:10 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace tpmodul6_1302210078
+public class SayaTubeVideo
 {
-    internal class Program
+    private int id;
+    private string title;
+    private int playCount = 0;
+
+    public SayaTubeVideo(string title)
     {
-        static void Main(string[] args)
-        {
-        }
+        Random random = new Random();
+        id = random.Next(1, 99999); // generate 5-digit random number
+        this.title = title;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: {0}", id);
+        Console.WriteLine("Judul: {0}", title);
+        Console.WriteLine("Play count: {0}", playCount);
+    }
+
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [HERDI_PRAKTIKAN]");
+        video.IncreasePlayCount(2);
+        video.IncreasePlayCount(9999);
+        video.PrintVideoDetails();
     }
 }
