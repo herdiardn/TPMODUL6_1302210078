0467fe85835af1a17f54c1274e72312be9b04492
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-03-26 14:40:06 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class SayaTubeVideo
 {
@@ -8,6 +9,8 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(title.Length <= 100 && title != null,
+            "Judul video memiliki panjang maksimal 100 karakter dan tidak berupa null");
         Random random = new Random();
         id = random.Next(1, 99999); // generate 5-digit random number
         this.title = title;
@@ -15,8 +18,22 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int playCount)
     {
-        this.playCount += playCount;
+        Debug.Assert(playCount < 10000000, 
+            "Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += playCount;
+            }
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);  
+        }
     }
+
 
     public void PrintVideoDetails()
     {
@@ -32,8 +49,9 @@ class program
     static void Main(string[] args)
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [HERDI_PRAKTIKAN]");
-        video.IncreasePlayCount(2);
-        video.IncreasePlayCount(9999);
+        for (int i = 0; i < 5; i++) {
+            video.IncreasePlayCount(11000000);
+        }
         video.PrintVideoDetails();
     }
 }
